bce66fdaee904fa0b5dccacf31e5b9424d7eed6c
Olena-Ignatenko <ignatenkoa2103@gmail.com>
2023-12-18 22:27:51 +0200

    Update task-01

--- js/task-01.js
+++ js/task-01.js
@@ -1,9 +1,8 @@
-const list = document.querySelector("#categories");
 const allLi = document.querySelectorAll(".item");
 console.log(`Number of categories: ${allLi.length}`);
 
 allLi.forEach((item) => {
-    const titleLi = document.querySelector("h2").textContent;
+    const titleLi = item.querySelector("h2").textContent;
     const elemLi = item.querySelectorAll("ul li").length;
 
     console.log(`Category: ${titleLi}`);
