8b02cc8957848330ecb8ea9b76f477ce0431ff3e
Olena-Ignatenko <ignatenkoa2103@gmail.com>
2023-12-18 22:33:12 +0200

    Update task-03

--- js/task-03.js
+++ js/task-03.js
@@ -5,11 +5,8 @@ inputText.addEventListener("input", (e) => {
     const inputTextContent = e.target.value.trim();
     if (inputTextContent === "") {
         spanText.textContent = "Anonymous";
-        console.log(inputTextContent);
     } else {
         spanText.textContent = inputTextContent;
-        
-        console.log(inputText);
 
     }
 
